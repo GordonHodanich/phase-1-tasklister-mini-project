16ef7eebd2910c6f01d1a81bd600a6841f467a1b
Gordon Hodanich <ghodanich@gmail.com>
2021-05-28 14:20:21 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+let form = document.querySelector('#create-task-form')
+let newEnteredTask = document.getElementById('new-task-description')
+
+  const newTask = (event) =>{
+    event.preventDefault()
+    let newTaskElement = document.createElement('li')
+    const listOfTasks = document.querySelector("#tasks")
+    listOfTasks.append(newTaskElement)
+    newTaskElement.innerText = newEnteredTask.value
+  }
+  form.addEventListener("submit" , newTask)
+})
+
